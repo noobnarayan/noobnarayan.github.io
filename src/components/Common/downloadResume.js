d3fea9c2369ededf2c713147eac5321575e9c4ac
Narayan Das <workmail.narayan@gmail.com>
2024-01-06 11:29:07 +0530

    clean repo

--- src/components/Common/downloadResume.js
+++
@@ -1,9 +0,0 @@
-import resume from "../../assets/Files/Narayan-Das-Resume.pdf";
-
-export const handleResumeDownload = () => {
-    const link = document.createElement("a");
-    link.download = "Narayan-Das-Resume.pdf";
-    link.href = resume;
-    link.click();
-    window.open("https://drive.google.com/file/d/1MDhdpGOMx_gb1R4b513W6X5QXtQJ5D8o/view?usp=sharing", "_blank");
-};
